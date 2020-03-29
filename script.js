6228e8acc26a29225813a0b275ae63f48ebeaed9
Mónica Polanco <mpolan16@g.holycross.edu>
2020-03-28 21:48:42 -0400

    made changes to debug function, will working on for loop

--- script.js
+++ script.js
@@ -1,87 +1,83 @@
 //VARIABLES
-  //7a create all the variables that hold my charaters
+  //Valiables that hold characters
   var charatersUPPER ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   var characterLower ="abcdefghijklmnopqrstuvwxyz"
   var characterSpecial="!@#$%^&*()"
   var characterNumber="1234567890"
 
-  var charaterSet=""
+  var passwordOnScreen = document.querySelector ("#password");
+
+  var charaterArray=[]
 //FUNCTIONS
 
-//characterSet select random charaters from this set for as long as the chosen charaterLength.
-
-
-//EVENTS
-document.getElementById("generate").onclick = function() {clickGeneratePopup()}; //not sure if this goes with the other piece.
-
-//THE PROGRAM I AM RUNNING
-
-//1. I want to user to click the "generate" button and for a popup to alert them.
+//when used clicks "Generate" popup asks quesitons.
 
 function clickGeneratePopup() {
-document.getElementById("generate").innerHTML = confirm ("Click 'OK' to generate random password");
-
-//2. I want when the user clicks ok for them to put in how long they want their password to be 8-128.
-  //2a.I want to store the answer in charaterLengthand alert what they chose.
+//Popup 1 how long they want their password to be 8-128.
+  //store password.
 var charaterLength = prompt("How many characters long would you like your password to be? (enter only whole numbers between 8 and 128");
-if (charaterLength != null) {
+
+if (charaterLength > 8 && charaterLength < 128) {
   alert("OK, your password will be " + charaterLength + " charaters long.");
+  }else {
+    alert ("invalid length");
+    //return if invalid
+    return;
+    
   }
-//3. I want user to choose if they want uppercase letters.
-    //a. store the answer!and alert what they chose.
+//Popup 2 choose if they want uppercase letters.
 var chooseCharaterUppercase = confirm ("Would you like: Uppercase Letters?");
 if (chooseCharaterUppercase ===true) {
-  //addcharatersUPPER to a pool of charaters...
-  charaterSet = characterSet+characterUPPER
+  //store answer + send to CharaterArray
+  charaterArray.push("upper")
   alert ("Uppercase letters will be added.")
 } else {
   alert ("No uppercase letters will be added.");
   }
-//4. I want user to choose if they want lowercase letters.
-    //a. store the answer!and alert what they chose.
+//Popup 3 choose if they want lowercase letters.
 var chooseCharaterLowercase = confirm ("Would you like: Lowercase Letters?");
 if (chooseCharaterLowercase ===true) {
-  charaterSet = characterSet+characterLower
+  //store answer + send to CharaterArray
+  charaterArray.push("lower")
   alert ("Lowercase letters will be added.")
 } else {
   alert ("No lowercase letters will be added.");
   }
-//5. I want user to choose if they want special characters.
-    //a. store the answer!and alert what they chose.
+//Popup 4 choose if they want special characters.
 var chooseSpecialCharater = confirm ("Would you like: Special Characters?");
 if (chooseSpecialCharater ===true) {
-  charaterSet = characterSet+characterSpecial
+  //store answer + send to CharaterArray
+  charaterArray.push("special")
   alert ("Special characters will be added.")
 } else {
   alert ("No special characters will be added.");
   }
-//6. I want user to choose if they want numbers.
-    //a. store the answer!and alert what they chose.
+//Popup 5 choose if they want numbers.
 var chooseNumbers = confirm ("Would you like: Numbers?");
 if (chooseNumbers ===true) {
-  charaterSet = characterSet+characterNumber
+  charaterArray.push("number")
+  //store answer + send to CharaterArray
   alert ("Numbers will be added.")
 } else {
   alert ("No Number will be added.");
 }
-//7. I want to create the password
-
-  //characterSet select random charaters from this set for as long as the chosen charaterLength.
+//select random charaters from all the selected user criteria and put in finalPass.
   var finalPass = ""
 
-for(var numberOfCharaters=0;numberOfCharaters<characterLength;numberOfCharaters++) {
-  var randomCharater=characterSet[math.floor(math.random()* charaterSet.length)];
-  finalPass=finalPass+randomCharater
+for (var i=0;i<characterLength;i++) { 
+  //use all selected criteria, then run each 1 time, get random charater, then keep going until you reach charater length.
+//if charaterArray[i] charaterType=charaterArray[i]
+  //else
+   var charaterType=charaterArray[Math.floor(Math.random()* charaterArray.length)];
 
-  console.log(finalPass)
+//if upper + random charaters from selected var to finalPAss
+// else if lower randomly select charater from lower var and add to finalPass
+//do for all
 }
+
+//Events
+document.getElementById("generate").onclick = function() {clickGeneratePopup()}; 
+
+//The password appears on HTML
+var passwordOnScreen= finalPass.value();
 }
-
-
-//8. I want the password to appear on the screen, not a popup.
-
-
-
-
-
-
